0f1d9e68fd323e845b1f95bb058b645c1276e63e
waste <madanmohan15073300@gmail.com>
2025-04-17 21:40:50 +0530

    Random Color Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
 import './App.css'
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return(
     <div className='App'>
-      {
-
-      }
+      
+      <RandomColor/>
       <Accordian/>
+      
     </div>
   )
 }
